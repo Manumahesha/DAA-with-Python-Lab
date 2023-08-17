a0f32beae8f2930811a77be7de6cf70a5be5a562
Mahadevaprasad <129720075+Manumahesha@users.noreply.github.com>
2023-08-17 22:33:38 +0530

    Create 9th(subjects).py

---
+++ 9th(subjects).py
@@ -0,0 +1,16 @@
+Enter the subjects: Math Physics Chemistry Biology
+Enter the name of students in Math: Alice Bob Charlie
+Enter the name of students in Physics: Alice Charlie David
+Enter the name of students in Chemistry: Bob Charlie Eve
+Enter the name of students in Biology: Alice David Eve
+0: Math
+1: Physics
+2: Chemistry
+3: Biology
+No of edges: 4
+Enter the edges [0 1]: 
+0 1
+0 2
+1 2
+1 3
+Minimum time slots required: 3
