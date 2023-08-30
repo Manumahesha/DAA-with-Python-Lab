59589eb5e364621bf4589578a9105287ef9bd468
Mahadevaprasad <129720075+Manumahesha@users.noreply.github.com>
2023-08-30 20:12:38 +0530

    Update obst.py

--- assignment/obst.py
+++ assignment/obst.py
@@ -1,26 +1,24 @@
-def optCost(freq, i, j):
-if j < i:
-return 0 
-if j == i:
-return freq[i]
-fsum = Sum(freq, i, j)
-Min = 999999999999
-for r in range(i, j + 1):
-cost = (optCost(freq, i, r - 1) + 
-optCost(freq, r + 1, j))
-if cost < Min:
-Min = cost
-return Min + fsum
-def optimalSearchTree(keys, freq, n):
-return optCost(freq, 0, n - 1)
-def Sum(freq, i, j): 
-s = 0
-for k in range(i, j + 1): 
-s += freq[k]
-return s
-if name == ' main ':
-keys = input("Enter keys separated by spaces: ").split()
-freq = list(map(float, input("Enter corresponding probabilities: ").split()))
-n = len(keys)
-print("Cost of Optimal BST is", 
-optimalSearchTree(keys, freq, n))
+def optimal_bst(keys, freq):
+    n = len(keys)
+    dp = [[0] * (n + 1) for _ in range(n + 1)]
+
+    for i in range(n):
+        dp[i][i + 1] = freq[i]
+        
+    for length in range(2, n + 1):
+        for i in range(n - length + 2):
+            j = i + length - 1
+            dp[i][j] = float('inf')
+            for r in range(i, j + 1):
+                cost = sum(freq[i:j + 1]) + (dp[i][r - 1] if r > i else 0) + (dp[r + 1][j] if r < j else 0)
+                if cost < dp[i][j]:
+                    dp[i][j] = cost
+    
+    return dp[0][n - 1]
+
+# Example usage
+keys = [10, 12, 20, 40]
+freq = [34, 8, 50, 23]
+
+result = optimal_bst(keys, freq)
+print("Minimum expected cost:", result)
