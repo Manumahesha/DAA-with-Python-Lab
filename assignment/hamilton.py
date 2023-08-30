465f70e242ed1fae98f63b236fdd5bb090a4e28e
Mahadevaprasad <129720075+Manumahesha@users.noreply.github.com>
2023-08-30 20:13:25 +0530

    Update hamilton.py

--- assignment/hamilton.py
+++ assignment/hamilton.py
@@ -1,36 +1,52 @@
-def is_valid(v, pos, path, graph): 
-if not graph[path[pos - 1]][v]:
-return False 
-if v in path:
-return False 
-return True
-def hamiltonian_cycle_util(graph, path, pos): 
-if pos == len(graph):
-if graph[path[pos - 1]][path[0]] == 1: 
-return True
-return False
-for v in range(1, len(graph)):
-if is_valid(v, pos, path, graph): 
-path[pos] = v
-if hamiltonian_cycle_util(graph, path, pos + 1): 
-return True
-path[pos] = -1
-return False
-def hamiltonian_cycle(graph): 
-path = [-1] * len(graph) 
-path[0] = 0
-if not hamiltonian_cycle_util(graph, path, 1): 
-print("No Hamiltonian cycle found.")
-else:
-print("Hamiltonian cycle found:", path)
-def main():
-n = int(input("Enter the number of nodes: ")) 
-graph = [[0] * n for _ in range(n)]
-print("Enter the adjacency matrix:") 
-for i in range(n):
-row = list(map(int, input().split())) 
-for j in range(n):
-graph[i][j] = row[j]
-hamiltonian_cycle(graph)
-if name == " main ": 
-main()
+def safeVertex(node):
+    if(node in path):
+        return False
+    
+    return True     
+
+def cycleDetection(E,n,root):
+    path.append(root)
+    for i in E[root]:
+       
+        if(safeVertex(i)):
+            if(cycleDetection(E,n,i)):
+                return True
+    
+    #Checking if our current path has all the vertices
+    if(len(path) == n):
+        if(path[0] in E[path[len(path)-1]]):
+            return True 
+        else:
+            return False
+   
+    path.pop()
+
+def HamiltonianCycle(E,n,root):
+    if(cycleDetection(E,n,root)):
+        print("True")
+    else:
+        print("False")
+
+
+
+path = []
+
+N_Vertices = int(input())
+
+matrix = list()
+for i in range(N_Vertices):
+    matrix.append([])
+
+N_Edges = int(input())
+
+for j in range(N_Edges):
+    edge_vertices = input().split()
+    u = int(edge_vertices[0])
+    v = int(edge_vertices[1])
+    matrix[u-1].append(v-1)
+    matrix[v-1].append(u-1)
+    
+HamiltonianCycle(matrix,N_Vertices,0)
+
+#This path is actually a Hamiltonian cycle.
+print(path)
