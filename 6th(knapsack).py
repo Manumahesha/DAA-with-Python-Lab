22fd9ab1b9ef5d4edbcf82f734a79ac769b5968c
Mahadevaprasad <129720075+Manumahesha@users.noreply.github.com>
2023-09-06 12:43:31 +0530

    Update 6th(knapsack).py

--- 6th(knapsack).py
+++ 6th(knapsack).py
@@ -1,4 +1,3 @@
-
 def knapsack_max_profit(weights,costs,capacity):
     num_items=len(weights)
     table=[[0]*(capacity+1) for _ in range(num_items+1)]
@@ -8,16 +7,15 @@ def knapsack_max_profit(weights,costs,capacity):
                 table[i][j] = max(costs[i-1]+table[i-1][j-weights[i-1]],table[i-1][j])  
             else:
                 table[i][j]=table[i-1][j]
-
-    selected_items=[]
+ selected_items=[]
     total_weight=capacity
     for i in range(num_items,0,-1):
         if table[i][total_weight]!=table[i-1][total_weight]:
             selected_items.append(i-1)
             total_weight-=weights[i-1]
     return table[num_items][capacity],selected_items
-weights = [int(i) for i in input("Enter the weights of the items: ").split(' ')] # 2 3 4 5
-costs = [int(i) for i in input("Enter the costs of the items: ").split(' ')] # 10 20 30 40
+weights = [int(i) for i in input("Enter the weights of the items: ").split(' ')]
+costs = [int(i) for i in input("Enter the costs of the items: ").split(' ')]
 capacity = int(input("Enter the capacity of the knapsack: ")) # 10
 max_profit,selected_items=knapsack_max_profit(weights,costs,capacity)
 print("maximun profit: ",max_profit)
@@ -25,10 +23,7 @@ print("selected coffee beans (index): ",selected_items)
 print("selected coffee beans (weights): ",[weights[i]for i in selected_items])
 print("selected coffee beans (costs): ",[costs[i]for i in selected_items])
 
-/*maximun profit:  70
-selected coffee beans (index):  [3, 1, 0]
-selected coffee beans (weights):  [5, 3, 2]
-selected coffee beans (costs):  [40, 20, 10]
+'''maximun profit:  70
 OUTPUT
 Enter the weights of the items: 2 3 4 5
 Enter the costs of the items: 10 20 30 40
@@ -36,4 +31,4 @@ Enter the capacity of the knapsack: 10
 maximun profit:  70
 selected coffee beans (index):  [3, 1, 0]
 selected coffee beans (weights):  [5, 3, 2]
-selected coffee beans (costs):  [40, 20, 10]/*
+selected coffee beans (costs):  [40, 20, 10]'''
