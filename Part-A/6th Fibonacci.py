fd0ea09db74df60e9fbd06967e3308d4e9d51da4
Mahadevaprasad <129720075+Manumahesha@users.noreply.github.com>
2023-09-08 12:31:39 +0530

    Update 6th Fibonacci.py

--- Part-A/6th Fibonacci.py
+++ Part-A/6th Fibonacci.py
@@ -1,10 +1,23 @@
-def fibonacci(n):
-    fib_series = [0, 1]
-    for i in range(2, n):
-        fib_series.append(fib_series[i - 1] + fib_series[i - 2])
-    return fib_series
+num = int(input("Enter the number upto which series should be printed: "))
+n1, n2 = 0, 1
+print("Fibonacci Series:", n1, n2, end=" ")
+for i in range(2, num):
+    n3 = n1 + n2
+    n1 = n2
+    n2 = n3
+    print(n3, end=" ")
+print()
 
+def fibonacciSeries(i):
+    if i <= 1:
+        return i
+    else:
+        return (fibonacciSeries(i - 1) + fibonacciSeries(i - 2))
 
-num = int(input("Enter the number: "))
-fibonacci_series = fibonacci(num)
-print("Fibonacci series for", num, ":", fibonacci_series)
+num = int(input("Enter the number upto which series should be printed: "))
+if num <=0:
+    print("Please enter a Positive Number")
+else:
+    print("Fibonacci Series:", end=" ")
+for i in range(num):
+    print(fibonacciSeries(i), end=" ")
