6aa7dee3a43edc6ea79a8c26d0dfe20d1c86e328
Mahadevaprasad <129720075+Manumahesha@users.noreply.github.com>
2023-09-08 12:26:45 +0530

    Update 2nd GCD.py

--- Part-A/2nd GCD.py
+++ Part-A/2nd GCD.py
@@ -1,9 +1,13 @@
-import math
-
-def gcd(a, b):
-    return math.gcd(a, b)
-
-num1 = int(input("Enter first num: "))
-num2 = int(input("Enter secoond num: "))
-result = gcd(num1, num2)
-print("GCD of", num1, "and", num2, "is", result)
+n=int(input("Enter number1:"))
+m=int(input("Enter number2:"))
+ans=0
+if n>m:
+    for i in range(1,n):
+        if n%i==0 and m%i==0:
+            ans=i
+        else:
+                for i in range(1,m):
+                    if n%i==0 and m%i==0:
+                        ans=i
+print("The GCD number is:",ans)
+                
